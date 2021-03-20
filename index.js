d9c3cb7327433bc292d5d759b43574b17c301072
keshavjindal <52865096+keshavjindal@users.noreply.github.com>
2021-03-20 15:12:17 +0530

    Initial files added.

---
+++ index.js
@@ -0,0 +1,65 @@
+var buttons = document.querySelectorAll(".drum");
+
+for(var i=0; i<buttons.length; i++)
+{
+    buttons[i].addEventListener("click" , function(){
+        makeSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+    });
+}
+
+document.addEventListener("keypress" , function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function buttonAnimation(key){
+    document.querySelector("." + key).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("." + key).classList.remove("pressed");
+    } , 100);
+}
+
+
+function makeSound(key){
+
+    if(key === "w")
+    {
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+    }
+    else if(key === "a")
+    {
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+    }
+    else if(key === "s")
+    {
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+    }
+    else if(key === "d")
+    {
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();   
+    }
+    else if(key === "j")
+    {
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+    }
+    else if(key === "k")
+    {
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+    }
+    else if(key == "l")
+    {
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+    }
+    else
+    {
+        // do nothing
+    }
+}
